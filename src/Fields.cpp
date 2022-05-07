fdcca8a0c5a2f97379a23c4fd1c2871fde36029d
Aditya <adityaphopale@gmail.com>
2022-05-07 15:39:39 +0200

    Calculating RS using fluid cells

--- src/Fields.cpp
+++ src/Fields.cpp
@@ -17,32 +17,32 @@ Fields::Fields(double nu, double dt, double tau, int imax, int jmax, double UI,
 }
 //_F(i,j) = Discretization::convection_u(U,i,j)
 void Fields::calculate_fluxes(Grid &grid) {
-  for(auto cell : grid.fluid_cells()){
-    int i = cell->i();
-    int j = cell->j();
+  // for(auto cell : grid.fluid_cells()){
+  //   int i = cell->i();
+  //   int j = cell->j();
 
-    _F(i,j) = _U(i, j) + _dt * (_nu * Discretization::diffusion(_U, i, j) -
+  //   _F(i,j) = _U(i, j) + _dt * (_nu * Discretization::diffusion(_U, i, j) -
+  //                                  Discretization::convection_u(_U, _V, i, j));
+  //   _G(i, j) = _V(i, j) + _dt * (_nu * Discretization::diffusion(_V, i, j) -
+  //                                  Discretization::convection_u(_U, _V, i, j));
+  // }
+
+
+
+
+  for (int i{1}; i < grid.imax(); i++) {
+    for (int j{1}; j < grid.jmax() + 1; j++) {
+      _F(i, j) = _U(i, j) + _dt * (_nu * Discretization::diffusion(_U, i, j) -
                                    Discretization::convection_u(_U, _V, i, j));
-    _G(i, j) = _V(i, j) + _dt * (_nu * Discretization::diffusion(_V, i, j) -
-                                   Discretization::convection_u(_U, _V, i, j));
+    }
   }
 
-
-
-
-  // for (int i{1}; i < grid.imax(); i++) {
-  //   for (int j{1}; j < grid.jmax() + 1; j++) {
-  //     _F(i, j) = _U(i, j) + _dt * (_nu * Discretization::diffusion(_U, i, j) -
-  //                                  Discretization::convection_u(_U, _V, i, j));
-  //   }
-  // }
-
-  // for (int i{1}; i < grid.imax() + 1; i++) {
-  //   for (int j{1}; j < grid.jmax(); j++) {
-  //     _G(i, j) = _V(i, j) + _dt * (_nu * Discretization::diffusion(_V, i, j) -
-  //                                  Discretization::convection_u(_U, _V, i, j));
-  //   }
-  // }
+  for (int i{1}; i < grid.imax() + 1; i++) {
+    for (int j{1}; j < grid.jmax(); j++) {
+      _G(i, j) = _V(i, j) + _dt * (_nu * Discretization::diffusion(_V, i, j) -
+                                   Discretization::convection_u(_U, _V, i, j));
+    }
+  }
 }
 
 void Fields::calculate_rs(Grid &grid) {
@@ -67,27 +67,27 @@ void Fields::calculate_rs(Grid &grid) {
 }
 
 void Fields::calculate_velocities(Grid &grid) {
-  for(auto cell : grid.fluid_cells()){
-    int i = cell->i();
-    int j = cell->j();
-    _U(i, j) = _F(i, j) - _dt * (_P(i + 1, j) - _P(i, j)) / grid.dx();
-    _V(i, j) = _G(i, j) - _dt * (_P(i, j + 1) - _P(i, j)) / grid.dy();
+  // for(auto cell : grid.fluid_cells()){
+  //   int i = cell->i();
+  //   int j = cell->j();
+  //   _U(i, j) = _F(i, j) - _dt * (_P(i + 1, j) - _P(i, j)) / grid.dx();
+  //   _V(i, j) = _G(i, j) - _dt * (_P(i, j + 1) - _P(i, j)) / grid.dy();
 
+  // }
+
+
+
+  for (int i{1}; i < grid.imax(); i++) {
+    for (int j{1}; j < grid.jmax() + 1; j++) {
+      _U(i, j) = _F(i, j) - _dt * (_P(i + 1, j) - _P(i, j)) / grid.dx();
+    }
   }
 
-
-
-  // for (int i{1}; i < grid.imax(); i++) {
-  //   for (int j{1}; j < grid.jmax() + 1; j++) {
-  //     _U(i, j) = _F(i, j) - _dt * (_P(i + 1, j) - _P(i, j)) / grid.dx();
-  //   }
-  // }
-
-  // for (int i{1}; i < grid.imax() + 1; i++) {
-  //   for (int j{1}; j < grid.jmax(); j++) {
-  //     _V(i, j) = _G(i, j) - _dt * (_P(i, j + 1) - _P(i, j)) / grid.dy();
-  //   }
-  // }
+  for (int i{1}; i < grid.imax() + 1; i++) {
+    for (int j{1}; j < grid.jmax(); j++) {
+      _V(i, j) = _G(i, j) - _dt * (_P(i, j + 1) - _P(i, j)) / grid.dy();
+    }
+  }
 }
 
 double Fields::calculate_dt(Grid &grid) {
