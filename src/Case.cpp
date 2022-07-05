33ab7466e56964e03b7ddbff1fb7c2422ace768a
Aditya <adityaphopale@gmail.com>
2022-07-05 21:22:18 +0200

    Debugging starts

--- src/Case.cpp
+++ src/Case.cpp
@@ -346,7 +346,7 @@ void Case::simulate() {
 
   // Following is the actual loop that runs till the defined time limit.
 
-  while (t <= 0) {
+  while (t <= _t_end) {
     // Calculating timestep for advancement to the next iteration.
     dt = _field.calculate_dt(_grid);
 
@@ -354,13 +354,7 @@ void Case::simulate() {
     _surface_boundaries->apply_pressure(_field, _grid);
     _surface_boundaries->apply_grey(_field, _grid);
 
-    for (int j = _grid.jmax(); j >= 0; j--) {
-      for (int i = 0; i < _grid.imax(); i++) {
-        std::cout << _field.p(i, j) << " ";
-      }
-      std::cout << "\n";
-    }
-
+    
     // output_vtk(0, Communication::rank);
 
     // Calculate new Temperatures
@@ -369,9 +363,13 @@ void Case::simulate() {
       Communication::communicate(_field.t_matrix(), _grid.domain());
     }
 
+    
+
     // Calculating Fluxes (_F and _G) for velocities in X and Y directions
     // respectively.
     _field.calculate_fluxes(_grid);
+
+    
 
     Communication::communicate(_field.f_matrix(), _grid.domain());
     Communication::communicate(_field.g_matrix(), _grid.domain());
@@ -407,10 +405,19 @@ void Case::simulate() {
       // logfile << "Residual: " << res << " Iteration:" << total_iter <<
     }
 
+    
+
     // Calculating updated velocities using pressure calculated in the
     // pressure poisson equation
     _field.calculate_velocities(_grid);
     //
+
+    // for (int j = _grid.jmax(); j >= 0; j--) {
+    //   for (int i = 0; i < _grid.imax(); i++) {
+    //     std::cout << _field.v(i, j) << " ";
+    //   }
+    //   std::cout << "\n";
+    // }
 
     for (int i = 0; i < _boundaries.size(); i++) {
       _boundaries[i]->apply(_field);
@@ -441,7 +448,9 @@ void Case::simulate() {
     }
 
     // Assign particle
+    std::cout<<_grid.fluid_cells().size()<<'\n';
     _grid.reset_fluid_cells();
+    std::cout<<_grid.fluid_cells().size()<<'\n';
 
     Communication::communicate(_field.u_matrix(), _grid.domain());
     Communication::communicate(_field.v_matrix(), _grid.domain());
@@ -562,10 +571,10 @@ void Case::output_vtk(int timestep, int rank) {
     for (int i = 0; i < _grid.domain().size_x + 1; i++) {
       vel[0] = (_field.u(i, j) + _field.u(i, j + 1)) * 0.5;
       vel[1] = (_field.v(i, j) + _field.v(i + 1, j)) * 0.5;
-      std::cout << vel[0] << " ";
+      //std::cout << vel[0] << " ";
       Velocity->InsertNextTuple(vel);
     }
-    std::cout << "\n";
+    //std::cout << "\n";
   }
 
   // Add Pressure to Structured Grid
